ba4a108bb1963aea00a01f7bce75918eb873bde9
tavoaqp <tavoaqp@gmail.com>
2013-09-08 10:56:27 -0300

    Commit inicial com criação de projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChallengeElo::Application.config.secret_key_base = 'f335b2eb34dac0423824358d15b294ea8a6d47a6ab7d4a27ebd6592d0ac3ee327b2eddfd62680226eeb619ddaf16b0b2970c35c2d2d64513ef343b36969fa55b'
