99d42545a83fbc433844638ceb667ba7e6b00775
tavoaqp <tavoaqp@gmail.com>
2013-09-23 12:43:24 -0300

    ajustando enviroment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-MediaChoice::Application.configure do
+ChallengeElo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
